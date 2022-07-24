9079db57c3331eab97a56102c053ba95dbde4e63
vish <vishpande2900@gmail.com>
2022-07-24 11:20:43 +0530

    sensor data to fb

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import json,os
 import requests
 import pyrebase
 import pandas as pd
+from datetime import date
 
 # creating a Flask app
 app = Flask(__name__)
@@ -22,45 +23,59 @@ config={
   "measurementId": "G-JFR16DHHN0"
   }
 
+firebase=pyrebase.initialize_app(config)
+db = firebase.database()
 
 @app.route('/', methods = ['GET'])
 def fetch_data():
-    firebase=pyrebase.initialize_app(config)
-    db = firebase.database()
-    raw = json.loads(json.dumps(db.child('data').get().val()))
-    dates_data = raw['dates']
-    dates_keys = dates_data.keys()
-    temp = []
-    for i in dates_keys:
-      d = {"date":i, "temp":dates_data[i]['Temp'], "turbidity":dates_data[i]['Turbidity'], 'water_lvl':dates_data[i]['Water level'] }
-      temp.append(d)
-    df = pd.DataFrame(temp)
     
-    #Line Chart of water temperature 
-    fig = px.line(df, x="date", y="temp", title='Water Temperature')
-    fig.write_image('static/img/temp.png')
+  raw = json.loads(json.dumps(db.child('data').get().val()))
+  dates_data = raw['dates']
+  dates_keys = dates_data.keys()
+  temp = []
+  for i in dates_keys:
+    d = {"date":i, "temp":dates_data[i]['Temp'], "turbidity":dates_data[i]['Turbidity'], 'water_lvl':dates_data[i]['Water level'] }
+    temp.append(d)
+  df = pd.DataFrame(temp)
+  
+  #Line Chart of water temperature 
+  fig = px.line(df, x="date", y="temp", title='Water Temperature')
+  fig.write_image('static/img/temp.png')
 
-    #water level bar graph
-    fig = px.bar(df, x='date',y='water_lvl')
-    fig.write_image('static/img/water_lvl.png')
+  #water level bar graph
+  fig = px.bar(df, x='date',y='water_lvl')
+  fig.write_image('static/img/water_lvl.png')
 
-    #Line chart for turbidity
-    fig = px.line(df, x='date',y='turbidity')
-    fig.write_image('static/img/tur.png')
+  #Line chart for turbidity
+  fig = px.line(df, x='date',y='turbidity')
+  fig.write_image('static/img/tur.png')
 
-    #PH gauge 
-    fig = go.Figure(go.Indicator(
-    mode = "gauge+number",
-    value = raw['ph'],
-    domain = {'x': [0, 1], 'y': [0, 1]},
-    title = {'text': "Ph Value"}))
-    fig.write_image('static/img/ph.png')
+  #PH gauge 
+  fig = go.Figure(go.Indicator(
+  mode = "gauge+number",
+  value = raw['ph'],
+  domain = {'x': [0, 1], 'y': [0, 1]},
+  title = {'text': "Ph Value"}))
+  fig.write_image('static/img/ph.png')
+  return render_template('index.html')
 
-
-
+@app.route('/update/<string:val>')
+def passdata(val):
+  raw = json.loads(json.dumps(db.child('data').get().val()))
+  a = date.today()
+  itab = val.split(':')
+  current_date = str(a.day)+'-'+'{:02d}'.format(a.month)+'-'+str(a.year)
+  val = val.split(':')
+  dates_data=raw['dates']
+  dates_data[current_date]['Temp'] = int(float(itab[0]))
+  dates_data[current_date]['Turbidity'] = int(float(itab[1]))
+  dates_data[current_date]['Water level'] = int(float(itab[2]))
+  data = {'ph':float((itab[3])),'date':dates_data}
+  db.child('data').update(data)
+  print(val)
+  return str(val)
     
-    return render_template('index.html')
-
+    
 
 
 
